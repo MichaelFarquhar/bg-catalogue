c995bbe07079612b2a8cd11cf90999b5b3a67b80
Michael Farquhar <michaelfarquhar4@gmail.com>
2024-02-02 15:11:29 -0500

    Add color theme and logo, and remove boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'th-primary': '#e56e0c',
+                'th-secondary': '#ebd6c3',
+                'th-accent': '#54433a',
+                'th-background': '#f3ede1',
+                'th-text': '#1d2544',
+            },
+        },
     },
     plugins: [],
 };
